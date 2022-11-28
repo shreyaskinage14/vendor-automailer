b9332fed9841ff8bcd959445d41def9dcac7fccc
shreyaskinage14 <shreyaskinage14@gmail.com>
2022-11-28 15:37:22 +0530

    template changes

--- api/mail.js
+++ api/mail.js
@@ -6,7 +6,6 @@ const path = require('path')
 router.post("/", async (req, res) => {
     try {
         let data = req.body
-        console.log(data);
         let smtptransport = nodemailer.createTransport({
             host: 'smtp.gmail.com',
             port: 465,
@@ -23,15 +22,12 @@ router.post("/", async (req, res) => {
         let mailOptions = {
             from: 'Shreyas Sanjay Kinage <shreyaskinage14@gmail.com>',
             to: data.email,
-            subject: `${data.name}, a Query has been raised in Vendor Portal`,
+            subject: `${data.name}, a Query for ${data.type} has been raised`,
             html: `
-        <h3>General Information</h3>
-        <ul>
-        <li>Name: ${data.name}</li>
-        <li>Email: xcage584@gmail.com</li>
-        </ul>
-        <h3>Query</h3>
-        <p>Testing the nodemailer</p>
+        <h3>${data.type} Query</h3>
+        <p>${data.query}</p>
+        <p>Please login to your vendor account and rectify the query asap.</p>
+        <a href="https://testing-omg.vercel.app/">Click to Open Vendor Portal</a>
         `
         }
 
@@ -43,11 +39,8 @@ router.post("/", async (req, res) => {
                 res.json({ message: `Email send to ${data.name} successfully`, messageID: info.messageId });
                 res.send({ message: `Email send to ${data.name} successfully`, messageID: info.messageId });
             }
-            console.log(err);
-            console.log(info);
         })
     } catch (error) {
-        console.log(error);
         return res.status(500).send("Server Error");
     }
 })
