7e0d478da5882e6bfba9e675e291accaa3a2a70d
shreyaskinage14 <68911342+shreyaskinage14@users.noreply.github.com>
2023-04-28 17:58:58 +0530

    vendorinboarding email added

--- api/mail.js
+++ api/mail.js
@@ -145,7 +145,7 @@ router.post("/approvemail", async (req, res) => {
         let mailOptions = {
             from: `${process.env.FROMEMAIL}`,
             // to: "naresh.chippa@omnicommediagroup.com",
-            to: `raghuraaman.janakiraman@omnicommediagroup.com`,
+            to: `raghuraaman.janakiraman@omnicommediagroup.com, ${process.env.FROMEMAIL}`,
             // to: "shreyaskinage14@gmail.com",
             subject: `Please Approve ${data.name} for Onboarding Portal`,
             html: `
