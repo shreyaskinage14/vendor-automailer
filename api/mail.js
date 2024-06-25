2e46ba649af3cd55a88cbe9f793246be3ba94b8f
shreyask14 <shreyaskinage14@gmail.com>
2024-06-25 23:56:21 +0530

    changed annalect approver email

--- api/mail.js
+++ api/mail.js
@@ -175,10 +175,11 @@ router.post("/approvemail", async (req, res) => {
         pass: process.env.EMAILPWD,
       },
     });
+    let _mailTo = data.introducerEmail?.includes("@annalect.com") ? "diwakar.gupta@annalect.com" : "preyash.parekh@omnicommediagroup.com";
     let mailOptions = {
       from: `${process.env.FROMEMAIL}`,
       // to: "naresh.chippa@omnicommediagroup.com",
-      to: `preyash.parekh@omnicommediagroup.com, ${process.env.FROMEMAIL}`,
+      to: `${_mailTo}, ${process.env.FROMEMAIL}`,
       // to: "shreyaskinage14@gmail.com",
       subject: `Please Approve ${data.name} for Onboarding Portal`,
       html: `
